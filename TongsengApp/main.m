306dbdd8c32d49068ecda7cd3db353917f08ac83
Fajran Iman Rusadi <fajran@gmail.com>
2009-08-15 22:44:10 +0200

    Add new Xcode project will all Tongseng files (except main.cpp) in it

---
+++ TongsengApp/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  TongsengApp
+//
+//  Created by Fajran Iman Rusadi on 8/15/09.
+//  Copyright __MyCompanyName__ 2009. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc,  (const char **) argv);
+}
